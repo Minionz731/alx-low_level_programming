16f957ec5d4ee582551cb410553c86a344701835
Minionz731 <mloyicz@gail.com>
2023-06-15 17:57:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-/**main-prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+#include <unistd.h>
+/**
+ * main-prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  * followed by a newline, to standard error
  * Return: always 1
  */
